ad6b70d6aaeb660ec8efd4b5d923ff0fb1248f48
MIKOI <83957083+MIKOIeu@users.noreply.github.com>
2021-11-25 21:15:38 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.card' , {
+    duartion: 5000,
+    origin: "bottom",
+    distance: "-100px"
+});
+
+sr.reveal('.logo' , {
+    duartion: 5000,
+    origin: "bottom",
+    distance: "-100px"
+});
+
