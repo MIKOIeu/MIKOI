a121fb890a89b5d381920eafdfed1bb9ffd2d92b
MIKOI <83957083+devMIKOI@users.noreply.github.com>
2022-01-03 10:46:08 +0100

    Add files via upload

---
+++ projects/urfront/js/main.js
@@ -0,0 +1,45 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.card' , {
+    delay: 300,
+    duartion: 5000,
+    origin: "bottom",
+    distance: "-500px"
+});
+
+sr.reveal('.image' , {
+    delay: 300,
+    duartion: 5000,
+    origin: "bottom",
+    distance: "-500px"
+});
+
+sr.reveal('.content' , {
+    delay: 300,
+    duartion: 5000,
+    origin: "bottom",
+    distance: "-500px"
+});
+
+sr.reveal('.textbox' , {
+    delay: 300,
+    duartion: 50,
+    origin: "left",
+    distance: "-500px"
+});
+
+sr.reveal('.secondimage' , {
+    delay: 300,
+    duartion: 5000,
+    origin: "right",
+    distance: "-500px"
+});
+
+sr.reveal('.contentbox' , {
+    delay: 300,
+    duartion: 5000,
+    origin: "top",
+    distance: "-500px"
+});
+
+alert("Strona nie jest narazie przystosowana pod urzadzenia mobilne!")
